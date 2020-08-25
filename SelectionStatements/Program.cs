d03cb400979e9aec97c29f4d96b9773c638d8f49
BK <brian_j_king@me.com>
2020-08-25 10:41:52 -0500

    First Commit BJKAUG2520'

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Transactions;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            Console.WriteLine("Enter Your Number:");
+            var userInput = int.Parse(Console.ReadLine());
+
+            //Create an if-statement that if the guessed number is below the initial value, print out “too low”.
+            //Create an else -if statement that if the number is higher than the initial value, print out “too high”.
+            //Create an else statement that prints out “Nevermind”.
+
+            if (userInput <= favNumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput >= favNumber)
+            {
+                Console.WriteLine("too high");
+            }
+            else 
+            {
+                Console.WriteLine("NeverMinds");
+            }
+
+        }
+    }
+}
